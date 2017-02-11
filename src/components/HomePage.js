e04b9ba10f239a518fc7ef5956941cacd19690c5
Daniele Damiani <danieledamiani@yahoo.it>
2017-02-11 10:41:57 +0000

    extract new order form in a component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,45 +1,18 @@
 import React from 'react';
 
+import NewOrderForm from './NewOrderForm';
 import ActiveOrders from './ActiveOrders';
 
 const HomePage = () => {
   return (
     <div className="col-xs-12">
       <h1 className="text-center">Live Order Board</h1>
-      <form className="form-inline vertical-padded" role="form">
-        <h4>New order</h4>
-        <div className="form-group">
-          <label htmlFor="selectUserId" className="control-label">
-            User Id
-          </label>
-          <select id="selectUserId" className="form-control">
-            <option>user1</option>
-            <option>user2</option>
-            <option>user3</option>
-            <option>user4</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <label htmlFor="inputQuantity" className="control-label">
-            Quantity
-          </label>
-          <input id="inputQuantity" className="form-control" type="number" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="inputPrice" className="control-label"> Price</label>
-          <input id="inputPrice" className="form-control" type="number" />
-        </div>
-        <div className="form-group">
-          <button type="submit" className="btn btn-success">Buy</button>
-        </div>
-        <div className="form-group">
-          <button type="submit" className="btn btn-primary">Sell</button>
-        </div>
-      </form>
+      <h4>New order</h4>
+      <NewOrderForm></NewOrderForm>
 
       <h4>Active orders</h4>
       <ActiveOrders></ActiveOrders>
-      
+
       <h4>Total Sell Orders</h4>
       <table className="table table-striped table-bordered">
         <thead>
